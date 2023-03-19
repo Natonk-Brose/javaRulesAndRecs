7857ad71be8d1c07d7dc8dd186639b5347b79289
AG202 <72565942+AG202@users.noreply.github.com>
2023-03-19 17:54:07 -0500

    Add files via upload

---
+++ Rules_Aneel/ERR08.java
@@ -0,0 +1,14 @@
+package IT355.Rules;
+
+public class ERR08 {
+    boolean nameConfirmation(String x, Object ApplicationName) {
+        /**
+         * Checking if the string variable is null.
+         * This will handle NullPointerException's
+         **/
+        if (x == null) {
+            return false;
+        }
+        return true;
+    }
+}
